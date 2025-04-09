07dd0dd966fc7d39b101d967673a7fc74f35ecff
Michael Barlow <michael@ramyun.co.uk>
2025-04-09 09:54:08 +0200

    add Rust benchmark

---
+++ benches/data/README.md
@@ -0,0 +1,5 @@
+# data
+
+`shakespeare.txt` is a concatenation of four Shakespear plays (Merchant of
+Venice, Romeo and Juliet, Macbeth, Hamlet). I got the text from here:
+https://github.com/cobanov/shakespeare-dataset/tree/main.
---
+++ benches/split.rs
@@ -0,0 +1,24 @@
+use divan::Bencher;
+
+use hat_splitter::{Splitter, HATSplitter};
+
+use divan::AllocProfiler;
+
+#[global_allocator]
+static ALLOC: AllocProfiler = AllocProfiler::system();
+
+fn main() {
+    divan::main();
+}
+
+fn hamlet_text() -> &'static str {
+    include_str!("data/shakespeare.txt")
+}
+
+#[divan::bench]
+fn split(bencher: Bencher) {
+    let splitter = HATSplitter::new();
+
+    bencher.bench(|| splitter.split(hamlet_text()));
+}
+
